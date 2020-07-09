0d225456c4f6cf63852fb04abd3b63383db919ca
AZIZ <aziz@AZIZs-MacBook-Air.local>
2020-07-09 15:03:56 +0200

    bu benim ilk dosyam

---
+++ ifpython.py
@@ -0,0 +1,6 @@
+name = input("Your name please?: ").title().strip()
+name1 = "Aziz"
+if name == name1:
+    print("Hello, {}! The password is : W@12".format(name))
+else:
+    print("Hello, {}! See you later.".format(name))
